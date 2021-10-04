3d78a1c5e3bf82a6c5c36aa6b13743e26d49c582
shienynursyifa <shienynursyifa@gmail.com>
2021-10-04 12:54:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,116 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 03</title>
 </head>
+
 <body>
     <h1>Asas PHP</h1>
-    
-    <?php 
+
+    <?php
     //komen satu baris
     #komen 1 baris
     /* komen
-    banyak baris*/ 
+    banyak baris*/
 
-    $namapelajar ='Shieny';
+    $namapelajar = 'Shieny';
     echo "<h2>Hello $namapelajar </h2>"; #output
 
     $hadir = false;
     $umur = 20;
     $nomat = '18DDT19F1131';
-     ?>
+    ?>
+    <?php
+    //addition
+    $x = 10;
+    $y = 6;
+
+    echo $x + $y;
+    echo "<br>";
+
+
+    //subtraction
+    $x = 10;
+    $y = 6;
+
+    echo $x - $y;
+    echo "<br>";
+
+    //multiplication
+    $x = 10;
+    $y = 6;
+
+    echo $x * $y;
+    echo "<br>";
+
+    //Division
+    $x = 10;
+    $y = 6;
+
+    echo $x / $y;
+    echo "<br>";
+
+    //Modulus
+    $x = 10;
+    $y = 6;
+
+    echo $x % $y;
+    echo "<br>";
+    ?>
+    <p>
+        <?php
+        $x = 5;
+        $y = 10;
+
+        echo ($x <=> $y); // returns -1 because $x is less than $y
+        echo "<br>";
+
+        $x = 15;
+        $y = 10;
+
+        echo ($x <=> $y); // returns 0 because values are equal
+        echo "<br>";
+
+        $x = 20;
+        $y = 10;
+
+        echo ($x <=> $y); // returns +1 because $x is greater than $y
+        ?>
+    </p>
+
+    <?php
+    $ibubapa = 'ibu'; # ibu /bapa/ kedua - duanya
+    $yatim = 500;
+    $piatu = 1000;
+
+    if ($ibubapa == 'ibu' xor $ibubapa = 'bapa') {
+        echo 'Anak yatim , dapat RM', $yatim;
+    } else {
+        #
+    }
+    ?>
+
+    <p>
+        <?php
+        echo 20 . 21;
+        ?>
+
+    </p>
+
+    <h2>
+        <?php
+        $bandar = 'Arau';
+        $baru = 'Bandar ';
+        for ($x = 0; $x < strlen($bandar); $x++) {
+            $baru .= $bandar[$x] . ' ';
+        }
+        echo $baru;
+        ?>
+    </h2>
 </body>
+
 </html>
